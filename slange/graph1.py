fb5e11360377df5df38d050e20f1007fa850bfa6
Haakon Hellebust <57912883+haakonnh@users.noreply.github.com>
2024-02-12 21:54:58 +0100

    Gimp

--- slange/graph1.py
+++ slange/graph1.py
@@ -1,12 +1,22 @@
 import pygraphviz as pgv
+from PIL import Image
 
-A=pgv.AGraph()
-A.add_node(1, shape='none', image='images/transformer-3p.png', fixedsize=True, imagescale=True, width=4, height=4)
-A.add_node(2, shape='none', image='images/queen.png', fixedsize=True, imagescale=True, width=4, height=4)
+size = 1.4
+#image1 = Image.open('images/transformer-3p.png')
+#img_rotated = image1.rotate(90)
+#img_rotated.save('images/traforotated.png')
+hdw_trafo = 276 / 210 # height/width because of cropping in Gimp
+
+A=pgv.AGraph(rankdir='LR', splines=False)
+A.add_node(1, shape='none', image='images/rook.png', fixedsize=True, imagescale=True, width=size, height=size)
+A.add_node(2, shape='none', image='images/img_rotated.png', fixedsize=True, imagescale=True, width=size, height=size*hdw_trafo)
+A.add_node(3, shape='none', image='images/queen.png', fixedsize=True, imagescale=True, width=size, height=size)
 
 A.add_edge(1,2)
+A.add_edge(2,3)
 
 
 A.layout(prog='dot')
+
 
 A.draw('file.png')
