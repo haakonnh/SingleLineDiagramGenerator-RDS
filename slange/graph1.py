7c4bbd5f634394c425cdb1c427e6a1a578e18678
misterkon <haakonhh7@gmail.com>
2024-02-13 13:53:04 +0100

    gogo

--- slange/graph1.py
+++ slange/graph1.py
@@ -2,13 +2,13 @@ import pygraphviz as pgv
 from PIL import Image
 
 size = 1.4
-#image1 = Image.open('images/transformer-3p.png')
-#img_rotated = image1.rotate(90)
-#img_rotated.save('images/traforotated.png')
+image1 = Image.open('images/Skillebryter.png')
+img_rotated = image1.rotate(90)
+img_rotated.save('images/Skillebryter.png')
 hdw_trafo = 276 / 210 # height/width because of cropping in Gimp
 
 A=pgv.AGraph(rankdir='LR', splines=False)
-A.add_node(1, shape='none', image='images/queen.png', fixedsize=True, imagescale=True, width=size, height=size)
+A.add_node(1, shape='none', image='images/Skillebryter.png', fixedsize=True, imagescale=True, width=size, height=size*hdw_trafo)
 A.add_node(2, shape='none', image='images/img_rotated.png', fixedsize=True, imagescale=True, width=size, height=size*hdw_trafo)
 A.add_node(3, shape='none', image='images/queen.png', fixedsize=True, imagescale=True, width=size, height=size)
 
