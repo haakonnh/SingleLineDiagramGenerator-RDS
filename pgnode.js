ca14672b7c6479cbfd9e1b1a7d032ba81a1cbe40
misterkon <haakonhh7@gmail.com>
2024-02-19 19:45:45 +0100

    PG-ing

--- pgnode.js
+++ pgnode.js
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv'
 import pkg from 'pg'
+
+
 
 const { Client } = pkg
 
@@ -6,12 +9,24 @@ const client = new Client({
       user: 'postgres',
       host: 'localhost',
       database: 'dabase',
-      password: 'Busten111',
+      password: dotenv.config().parsed.HAAKON_PASSORD,
       port: 5432,
 });
 
-await client.connect()
 
-console.log(await client.query('SELECT * FROM test;'))
+await client.connect();
 
-await client.end()
+//await client.query("INSERT INTO test VALUES ('Top.');");
+//await client.query("INSERT INTO test VALUES ('Top.JE1');");
+
+//await client.query('DELETE FROM test;');
+
+let resp = await client.query('SELECT path FROM test;');
+
+console.log(resp.rowCount);
+
+resp.rows.forEach(row => {
+      console.log(row)
+});
+
+await client.end();
