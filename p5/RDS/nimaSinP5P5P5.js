52617a53e6be8c19548efc0db10878dba55cfcda
forvirraKurder <125813513+forvirraKurder@users.noreply.github.com>
2024-02-22 12:53:31 +0100

    hei

--- p5/RDS/nimaSinP5P5P5.js
+++ p5/RDS/nimaSinP5P5P5.js
@@ -9,7 +9,11 @@ let data = {};
 
 let fetchedData = {};
 
+let fetchedData1 = {};
+
 let dataArray = [];
+
+let dataArray1 = [];
 
 let components = [];
 
@@ -21,13 +25,20 @@ let loadedImages = {
 
 async function fetchAndProcessData() {
       const apiUrl = 'http://localhost:8000/diagram_data_connections'; // Replace with your endpoint
+
+      const apiUrl1 = 'http://localhost:8000/diagram_data'; // Replace with your endpoint
       try {
             const response = loadJSON(apiUrl);
+            const response1 = loadJSON(apiUrl1);
             
             const data = response;
+            const data1 = response1;
+
+            console.log("Data from API:", data1);
             console.log("Data from API:", data);
 
             fetchedData = data;
+            fetchedData1 = data1;
       }     
       catch (error) {
             console.error("Error fetching data:", error);
@@ -54,16 +65,25 @@ const pattern = /[A-Za-z]+/;
 
 function setup() {
       Object.entries(fetchedData).forEach(([key, value]) => {
-            dataArray.push(value[0]);
+            dataArray.push(value);
       });
 
-      dataArray.forEach(element => {
+      Object.entries(fetchedData1).forEach(([key, value]) => {
+            dataArray1.push(value);
+      });
+
+/*       dataArray.forEach(element => {
         console.log(element);
         });
+
+        dataArray1.forEach(element => {
+            console.log(element);
+            }); */
 
       createCanvas(1425, 725);
       background(255);
       console.log("ARRAY!:", dataArray);
+      console.log("ARRAY!:", dataArray1);
 
       Object.entries(data).forEach(([key, value]) => {
             components.push(value.path.split('.').pop());
@@ -82,27 +102,33 @@ function setup() {
                   // draw line between imgs
                   newComponents.push(match[0]);
             }
+            
       });
+      console.log("Element:", newComponents);
+
       let lastCoords = {x: 50, y: 50};
       let imgIndex = 0;
-      for (let i = 0; i < newComponents.length; i++) {
+      
+      /* for (let i = 0; i < newComponents.length; i++) {
             
             if (newComponents[i] == "JE") { 
                   stroke(0);
                   strokeWeight(2);
-                  lineCoords = toCoords(lastCoords.x, lastCoords.y);
-                  line(lineCoords.x, lineCoords.y, lineCoords.x + size, lineCoords.y);
+                  //lineCoords = toCoords(lastCoords.x, lastCoords.y);
+                  //line(lineCoords.x, lineCoords.y, lineCoords.x + size, lineCoords.y);
                   lastCoords = {x: lastCoords.x + size, y: lastCoords.y};
                   //line(lineCoords.x, lineCoords.y, lineCoords.x, lineCoords.y + size / 2);
                   //lastCoords = {x: lastCoords.x, y: lastCoords.y + size / 2};
             } else {
-                  console.log(imgIndex, imgs[imgIndex], newComponents[i]);
+                  //console.log(imgIndex, imgs[imgIndex], newComponents[i]);
                   
-                  image(imgs[imgIndex], lastCoords.x, lastCoords.y, size, size);
+                  //image(imgs[imgIndex], lastCoords.x, lastCoords.y, size, size);
                   lastCoords = {x: lastCoords.x + size, y: lastCoords.y}; 
                   imgIndex++;
             }
-      }
+      } */
+
+      
       
 }
 
