23699a5f73549652ae267ec27499c7b085c9242f
forvirraKurder <125813513+forvirraKurder@users.noreply.github.com>
2024-02-28 12:10:56 +0100

    jævla bra skillebryter

--- p5/RDS/nimaape.py
+++ p5/RDS/nimaape.py
@@ -1,4 +1,4 @@
-from typing import Union
+""" from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import psycopg
@@ -52,4 +52,73 @@ def get_diagram_data_connections():
                   cur.execute('SELECT * FROM connections')
                   data = cur.fetchall()
                   return data
-            
+             """
+
+
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import psycopg
+import os
+import json
+
+
+# API for p5.js to fetch PostgreSQL data
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  
+    # Update with your p5.js development origins
+    # ... add any other allowed origins
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+);
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# TODO: Suck data from PostgreSQL and return it to p5.js
+
+@app.get("/diagram_data")
+def get_diagram_data():
+      with psycopg.connect(dbname="banenor.sorberg-nypan", user="postgres") as conn:
+            with conn.cursor() as cur:
+                  cur.execute('SELECT id, path1 FROM tree')
+                  results = cur.fetchall()
+                  
+                  data = [
+                        {
+                              "id": result[0],
+                              "path": result[1]
+                        }
+                        for result in results
+                  ]
+                  return data
+
+@app.get("/relation_data")
+def get_relation_data():
+      with psycopg.connect(dbname="banenor.sorberg-nypan", user="postgres") as conn:
+            with conn.cursor() as cur:
+                  cur.execute('SELECT * FROM connections')
+                  results = cur.fetchall()
+                  data = [
+                        {
+                              "rel_id": result[0],
+                              "node1": result[1],
+                              "node2": result[2],
+                        }
+                        for result in results
+                  ]
+                  # loop through each component fetched and fetch connections
+                  return data
--- p5/RDS/structs.js
+++ p5/RDS/structs.js
@@ -158,9 +158,57 @@ class SectionIsolator {
       }
 }
 
-class Skillebryter {
-      constructor(x1, y1, x2, y2, avEllerPå = 0) {}
-      draw() {}
+class SkillebryterOgSeksjon {
+    constructor(x1, y1, x2, y2, avEllerPå = 0) {
+        // coords for the last line
+        this.lastX1 = x1
+        this.lastX2 = x2
+        this.lastY1 = y1
+        this.lastY2 = y2
+
+        // slope, angle and length of the last line
+        this.lastSlope = (y2 - y1) / (x2 - x1)
+        this.angle = atan2(y2 - y1, x2 - x1)
+        this.length = dist(x1, y1, x2, y2) / 6
+
+        // the desired angle of the upper line
+        this.newAngle = this.angle + radians(-60)
+
+        // calculated coords for the upper line
+        this.upperX = this.lastX2 + cos(this.newAngle) * this.length
+        this.upperY = this.lastY2 + sin(this.newAngle) * this.length
+
+        // pytahgorean theorem to calculate the coords for the lower line
+        this.mot = sin(radians(-60)) * this.length
+        this.hos = sqrt(this.length * this.length - this.mot * this.mot)
+        this.lowerX1 = x2 + cos(this.angle) * this.hos
+        this.lowerY1 = y2 + sin(this.angle) * this.hos
+        this.lowerAngle = this.newAngle + radians(180) // angle flipped by 180 degrees for lower line
+        this.lowerX2 = this.lowerX1 + cos(this.lowerAngle) * this.length
+        this.lowerY2 = this.lowerY1 + sin(this.lowerAngle) * this.length
+
+        // output coords for "last element", connection points essentially
+        this.connectionX1 = this.lowerX1
+        this.connectionY1 = this.lowerY1
+        this.connectionX2 = this.lowerX2
+        this.connectionY2 = this.lowerY2
+
+
+        // for skillebryter
+        this.skillebryterX1 = this.lastX2
+    }
+
+  draw() {
+        // draw upper and lower line
+        line(this.lastX2, this.lastY2, this.upperX, this.upperY)
+        line(this.lowerX1, this.lowerY1, this.lowerX2, this.lowerY2)
+
+        line(this.lastX2 - myDistX/5, this.lastY2, this.lastX2 - myDistX/5, this.lastY2 - 1.5*this.length)
+        line(this.lowerX1 + myDistX/5, this.lowerY1, this.lowerX1 + myDistX/5, this.lowerY1 - 1.5*this.length)
+
+        line(this.lastX2-myDistX/5, this.lastY2 - 1.5*this.length, this.lowerX1+myDistX/5, this.lowerY1 - 1.5*this.length)
+        line(this.lastX2 + myDistX/7, this.lastY2 - 1.5*this.length + myDistY/7, this.lastX2 + myDistX/7 , this.lowerY1 - 1.5*this.length - myDistY/7)
+    }
 }
 
 
@@ -221,7 +269,7 @@ class Trafosamling {
 
 const componentToPath = {
       "UAA": Section,
-      "QBA": Skillebryter,
+      "QBA": SkillebryterOgSeksjon, //Endrer denne til SkillebryterOgSeksjon, men må finne en bedre løsning
       "TAA": Trafo,
       "WBC": Line,
       "FCA": Sikring,
